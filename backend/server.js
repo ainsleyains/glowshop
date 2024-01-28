6b2e851edd8dafcfa1016f412c9f6bcbc423d6a9
Ainsley <ainsleyjyl@gmail.com>
2024-01-27 19:22:37 -0800

    fetch from database

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config(); // config before using port
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((el) => el._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
