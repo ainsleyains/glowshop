6cea90f00da1c14ecb14b81ef9a6c8e7480f42a2
Ainsley <ainsleyjyl@gmail.com>
2024-01-30 10:37:38 -0800

    add Loader & Message Components

--- front-end/src/pages/ProductPage.jsx
+++ front-end/src/pages/ProductPage.jsx
@@ -1,21 +1,14 @@
 import { useParams, Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 export default function ProductPage() {
-    const [product, setProduct] = useState({});
     const { id: productId } = useParams();
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const { data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        };
-        fetchData();
-    }, [productId]);
+    const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
     return (
         <>
@@ -25,60 +18,65 @@ export default function ProductPage() {
             >
                 <FaArrowLeft /> Back
             </Link>
-            <Row>
-                <Col md={6}>
-                    <Image
-                        src={product.image}
-                        alt={product.name}
-                        fluid
-                    />
-                </Col>
-                <Col md={1}></Col>
-                <Col md={4}>
-                    <ListGroup className='list-group-flush'>
-                        <ListGroup.Item>
-                            <p>{product.collection}</p>
-                            <h3>{product.name}</h3>
-                            <Rating
-                                value={product.rating}
-                                text={`${product.numReviews}reviews`}
-                            />
-                        </ListGroup.Item>
-                        {/* <ListGroup.Item>
+            {isLoading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>{error?.data.message || error.error}</Message>
+            ) : (
+                <Row>
+                    <Col md={6}>
+                        <Image
+                            src={product.image}
+                            alt={product.name}
+                            fluid
+                        />
+                    </Col>
+                    <Col md={1}></Col>
+                    <Col md={4}>
+                        <ListGroup className='list-group-flush'>
+                            <ListGroup.Item>
+                                <p>{product.collection}</p>
+                                <h3>{product.name}</h3>
+                                <Rating
+                                    value={product.rating}
+                                    text={`${product.numReviews}reviews`}
+                                />
+                            </ListGroup.Item>
+                            {/* <ListGroup.Item>
                             <Rating
                                 value={product.rating}
                                 text={`${product.numReviews}reviews`}
                             />
                         </ListGroup.Item> */}
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Price:</Col>
-                                <Col>${product.price}</Col>
-                            </Row>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Status:</Col>
-                                <Col>
-                                    <stong>
-                                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                                    </stong>
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Button
-                                className='btn-black'
-                                type='button'
-                                disabled={product.countInStock === 0}
-                            >
-                                Add To Cart
-                            </Button>
-                        </ListGroup.Item>
-                        <ListGroup.Item>{product.description}</ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                {/* <Col md={3}>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>${product.price}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        <stong>
+                                            {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                                        </stong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button
+                                    className='btn-black'
+                                    type='button'
+                                    disabled={product.countInStock === 0}
+                                >
+                                    Add To Cart
+                                </Button>
+                            </ListGroup.Item>
+                            <ListGroup.Item>{product.description}</ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    {/* <Col md={3}>
                     <Card>
                         <ListGroup className='list-group-flush'>
                             <ListGroup.Item>
@@ -109,7 +107,8 @@ export default function ProductPage() {
                         </ListGroup>
                     </Card>
                 </Col> */}
-            </Row>
+                </Row>
+            )}
         </>
     );
 }
