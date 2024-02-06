9e40702464c5b25497ff01061c6ce483d9c12f15
Ainsley <ainsleyjyl@gmail.com>
2024-02-06 12:22:57 -0800

    add Order Route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
@@ -17,6 +19,7 @@ app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
