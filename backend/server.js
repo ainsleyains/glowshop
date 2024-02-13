b9a13d6b3cbd94484fd2f0a7a8a23e4fc4217e2b
Ainsley <ainsleyjyl@gmail.com>
2024-02-13 15:24:36 -0800

    get deployed

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,17 @@ app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAY
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/front-end/build')));
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running');
+    });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
